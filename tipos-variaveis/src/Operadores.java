405e53e3c53c3c9967d0b649b10a763411ec5937
TndTanada <tnd.tanada@gmail.com>
2024-07-05 13:22:42 -0300

    Implementando Operadores e corrigindo TiposVariaveis.java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,48 @@
+public class Operadores {
+    public static void main(String[] args) {
+    /* String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+        
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);*/ 
+
+        //operadores unários
+        
+        int numero = 5;
+
+        System.out.println(-numero);
+
+        System.out.println(numero);
+
+        int numero2 = 10;
+
+        numero2 = - numero2;
+
+        System.out.println(numero2);
+
+        numero2 = numero2 * -1;
+
+        System.out.println(numero2);
+
+    }
+    
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,11 +1,11 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         
         // TiposEVariaveis.java
 
-        short numeroCurto = 1;
+        /*short numeroCurto = 1;
         int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal; // (short) força uma mudança de tipo
+        short numeroCurto2 = (short) numeroNormal; // (short) força uma mudança de tipo*/
 
         int numero = 5;
         
@@ -16,7 +16,7 @@ public class TiposVariaveis {
         //final double VALOR_DE_PI = 3.14;
 		
         double VALOR_DE_PI = 3.14;
-		VALOR_DE_PI=3.15; //Esta linha vai apresentar erro de compilação! O IDE solicita que seja subtraída o termo 'final' na declaração da variável
+		//VALOR_DE_PI=3.15; //Esta linha vai apresentar erro de compilação! O IDE solicita que seja subtraída o termo 'final' na declaração da variável
         //declarar uma variável como uma constante, utilizamos a palavra 'final', mas por convenção, esta variável deverá ser escrita toda em CAIXA ALTA.
     }
 }
